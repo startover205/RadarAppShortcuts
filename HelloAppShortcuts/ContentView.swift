dd0e4cd45bdb249249bee4c8a3002d602c7bf1f9
Ming-Ta Yang <startover205@gmail.com>
2023-10-13 13:01:59 +0800

    Initial Commit

---
+++ HelloAppShortcuts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloAppShortcuts
+//
+//  Created by Ming-Ta Yang on 2023/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloAppShortcuts/HelloAppShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppShortcutsApp.swift
+//  HelloAppShortcuts
+//
+//  Created by Ming-Ta Yang on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
